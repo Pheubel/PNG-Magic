64bed84856aacec21d0524d5f7abd8a0fb54b404
Pheubel <luca.guagliardo@live.nl>
2022-01-14 13:03:23 +0100

    Created start

---
+++ PngMagic/PngMagic/OperationMode.cs
@@ -0,0 +1,28 @@
+﻿namespace PngMagic.Core;
+
+/// <summary>
+/// Defines how the core should handle the PNG
+/// </summary>
+public enum OperationMode
+{
+    /// <summary>
+    /// No operation is specified, fail.
+    /// </summary>
+    Unspecified,
+    /// <summary>
+    /// Append an image as custom chunks.
+    /// </summary>
+    Pack,
+    /// <summary>
+    /// Extracts an image from the custom chunk data.
+    /// </summary>
+    Extract,
+    /// <summary>
+    /// Removes an image from the custom chunk data.
+    /// </summary>
+    Remove,
+    /// <summary>
+    /// projects the hidden image onto the original image.
+    /// </summary>
+    Project
+}
