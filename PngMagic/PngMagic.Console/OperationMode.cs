318c92b2528064ee415fb3977ad4591ce067747c
Pheubel <luca.guagliardo@live.nl>
2022-01-15 05:56:17 +0100

    use file declared namespace

--- PngMagic/PngMagic.Console/OperationMode.cs
+++ PngMagic/PngMagic.Console/OperationMode.cs
@@ -1,33 +1,32 @@
-﻿namespace PngMagic.Console
+﻿namespace PngMagic.Console;
+
+/// <summary>
+/// Defines how the core should handle the PNG
+/// </summary>
+public enum OperationMode
 {
     /// <summary>
-    /// Defines how the core should handle the PNG
+    /// No operation is specified, fail.
     /// </summary>
-    public enum OperationMode
-    {
-        /// <summary>
-        /// No operation is specified, fail.
-        /// </summary>
-        Unspecified,
+    Unspecified,
 
-        /// <summary>
-        /// Append an image as custom chunks.
-        /// </summary>
-        Pack,
+    /// <summary>
+    /// Append an image as custom chunks.
+    /// </summary>
+    Pack,
 
-        /// <summary>
-        /// Extracts an image from the custom chunk data.
-        /// </summary>
-        Extract,
+    /// <summary>
+    /// Extracts an image from the custom chunk data.
+    /// </summary>
+    Extract,
 
-        /// <summary>
-        /// Removes an image from the custom chunk data.
-        /// </summary>
-        Remove,
+    /// <summary>
+    /// Removes an image from the custom chunk data.
+    /// </summary>
+    Remove,
 
-        /// <summary>
-        /// projects the hidden image onto the original image.
-        /// </summary>
-        Project
-    }
+    /// <summary>
+    /// projects the hidden image onto the original image.
+    /// </summary>
+    Project
 }
