037e0aa51a0c30d3185058a88edbea1b004828df
Pheubel <luca.guagliardo@live.nl>
2022-01-15 18:24:48 +0100

    Added new helper function

--- PngMagic/PngMagic/Utils.cs
+++ PngMagic/PngMagic/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace PngMagic.Core;
 
@@ -6,13 +7,23 @@ internal static class Utils
 {
     public static readonly byte[] DataChunkType = Encoding.UTF8.GetBytes("IDAT");
     public static readonly byte[] InjectedChunkType = Encoding.UTF8.GetBytes("ijNK");
+
     static readonly byte[] PngHeader = new byte[] { 137, 80, 78, 71, 13, 10, 26, 10 };
 
-    public static bool IsPNG(ReadOnlySpan<byte> fileData) => fileData[..PngHeader.Length].SequenceEqual(PngHeader);
+    public static bool IsPNG(ReadOnlySpan<byte> fileData) =>
+        fileData[..PngHeader.Length].SequenceEqual(PngHeader);
 
     public static void ReverseOnLittleEndian(this Span<byte> bytes)
     {
         if (BitConverter.IsLittleEndian)
             bytes.Reverse();
+    }
+
+    public static uint GetInjectedChunkTypeValue()
+    {
+        Span<byte> buffer = stackalloc byte[sizeof(uint)];
+        InjectedChunkType.CopyTo(buffer);
+        buffer.ReverseOnLittleEndian();
+        return MemoryMarshal.Read<uint>(buffer);
     }
 }
